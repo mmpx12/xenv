0f0034b2abee9133f057b29b6a31d926e933d647
drclaw <mmpx09@protonmail.com>
2022-11-14 07:39:21 +0000

    prevent race condition

--- xenv.go
+++ xenv.go
@@ -15,13 +15,14 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/mmpx12/optionparser"
 )
 
 var (
-	success   = 0
+	success   int32
 	mu        = &sync.Mutex{}
 	thread    = make(chan struct{}, 50)
 	wg        sync.WaitGroup
@@ -58,28 +59,31 @@ func CheckEnv(client *http.Client, url, path string) {
 		<-thread
 		return
 	}
-	match, err := regexp.MatchString(`(?m)<body|<script|<html>`, string(body))
+	match, err := regexp.MatchString(`(?mi)<body|<script|<html>|<?php`, string(body))
 	if err != nil {
 		<-thread
 		return
 	}
-	if len(body) >= 3500 || match {
+	if len(body) >= 3700 || match {
 		<-thread
 		return
 	}
-	r := regexp.MustCompile(`(?m)^([A-Za-z0-9-]|[-_]|^[\s\.]){1,30}[\s]?=.{1,60}`)
+	//r := regexp.MustCompile(`(?m)^([A-Za-z0-9-]|[-_#]|^[\s\.]){1,35}[\s]?\=.{1,100}`)
+	r := regexp.MustCompile(`(?m)^([A-Za-z0-9#-_]){1,35}[\s]{0,10}=.{2,100}$`)
 	if r.MatchString(string(body)) {
 		all := r.FindAllString(string(body), -1)
 		if len(all) > 5 {
 			mu.Lock()
-			success++
-			WriteToFile(resp.Request.URL.String())
-			mu.Unlock()
-			fmt.Println("\033[1K\rENV FOUND:\033[36m", resp.Request.URL.String()+"\033[0m")
+			//success++
+			atomic.AddInt32(&success, 1)
+			WriteToFile("============================\n" + resp.Request.URL.String())
+			fmt.Println("\033[1K\r\033[32mENV FOUND:\033[36m", resp.Request.URL.String()+"\033[0m")
 			for _, j := range all {
-				v := strings.Split(j, "=")
-				fmt.Println("\033[33m" + v[0] + "\033[0m=\033[35m" + v[1])
+				key, val, _ := strings.Cut(j, "=")
+				fmt.Printf("\033[33m%s\033[37m=\033[35m%s\n", key, val)
+				WriteToFile(key + "=" + val)
 			}
+			mu.Unlock()
 			<-thread
 			return
 		}
@@ -170,12 +174,14 @@ func main() {
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
 	i := 0
-	total := LineNBR(input)
+	total := LineNBR(input) * len(path)
 	for fileScanner.Scan() {
-		i++
 		target := fileScanner.Text()
-		fmt.Printf("\033[1K\r\033[31m[\033[33m%d\033[36m/\033[33m%d \033[36m(\033[32m%d\033[36m)\033[31m] \033[35m%s\033[0m", i, total, success, target)
 		for _, p := range path {
+			i++
+			mu.Lock()
+			fmt.Printf("\033[1K\r\033[31m[\033[33m%d\033[36m/\033[33m%d \033[36m(\033[32m%d\033[36m)\033[31m] \033[0m%s%s\033[0m", i, total, int(success), target, p)
+			mu.Unlock()
 			thread <- struct{}{}
 			wg.Add(1)
 			go CheckEnv(client, target, p)
